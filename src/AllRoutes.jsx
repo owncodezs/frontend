d2cf690a8983d86bd17f57b46c2042abfbe620b6
Manibharathi Murugesan <76097762+itsmanibharathi@users.noreply.github.com>
2023-05-03 19:37:03 +0530

    renew allRoutes

--- src/AllRoutes.jsx
+++ src/AllRoutes.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import {HashRouter as Router,  Route} from 'react-router-dom'
+import {Routes,  Route} from 'react-router-dom'
 import Contact from './pages/Contact/Contact'
 import LandingPage from './pages/LandingPage/LandingPage'
+import PageNotFound from './pages/PageNotFound/PageNotFound'
 import AboutPage from './pages/AboutPage/AboutPage'
 import ServicePage from './pages/ServicePage/ServicePage'
 import GalleryPage from './pages/GalleryPage/GalleryPage'
@@ -16,6 +17,7 @@ function AllRoutes() {
         <Route path='/service' element={<ServicePage />} />
         <Route path='/gallery' element={<GalleryPage />} />
         <Route path='/contact' element={<Contact />} />
+        <Route path='*' element={<PageNotFound />} />
     </Routes>  
   )
 }
